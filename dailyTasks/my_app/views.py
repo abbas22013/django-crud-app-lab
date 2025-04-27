96613206a7a6da1051134375dff23b2f9f3c3330
Abbas <abbas22013@gmail.com>
2025-04-27 09:29:40 +0300

    add templates

--- dailyTasks/my_app/views.py
+++ dailyTasks/my_app/views.py
@@ -4,30 +4,29 @@ from .models import Task
 # my_app/views.py
 
 
-# صفحة البداية (Home)
 def home(request):
     return render(request, 'my_app/home.html')
 
-# عرض جميع المهام
+
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'my_app/index.html', {'tasks': tasks})
 
-# عرض تفاصيل المهمة
+
 def detail(request, id):
     task = get_object_or_404(Task, id=id)
     return render(request, 'my_app/detail.html', {'task': task})
 
-# إنشاء مهمة جديدة
+
 def create(request):
     if request.method == 'POST':
         title = request.POST['title']
         description = request.POST['description']
         Task.objects.create(title=title, description=description)
-        return redirect('task_list')  # العودة إلى صفحة قائمة المهام بعد الإنشاء
+        return redirect('task_list') 
     return render(request, 'my_app/create.html')
 
-# تعديل مهمة موجودة
+
 def edit(request, id):
     task = get_object_or_404(Task, id=id)
     if request.method == 'POST':
@@ -35,11 +34,11 @@ def edit(request, id):
         task.description = request.POST['description']
         task.completed = 'completed' in request.POST
         task.save()
-        return redirect('task_list')  # العودة إلى صفحة قائمة المهام بعد التعديل
+        return redirect('task_list')  
     return render(request, 'my_app/edit.html', {'task': task})
 
-# حذف مهمة
+
 def delete(request, id):
     task = get_object_or_404(Task, id=id)
     task.delete()
-    return redirect('task_list')  # العودة إلى صفحة قائمة المهام بعد الحذف
+    return redirect('task_list') 
